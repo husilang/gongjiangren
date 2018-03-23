52fe8cb6a77f1f0c11047f94992fe2d2efcdc20e
husilang <601906911@qq.com>
2018-03-23 15:50:23 +0800

    注册步骤 项目新增修改

--- layouts/firmcenter.vue
+++ layouts/firmcenter.vue
@@ -50,18 +50,18 @@
   .el-carousel__item h3 {
     color: #475669;
     font-size: 14px;
-    height: 70px;
+    height: 64px;
     opacity: 0.75;
     margin: 0;
     img {width: 100%;}
   }
 
   .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
+    background-color: #c0ccda;
   }
 
   .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
+    background-color: #c0ccda;
   }
   .firm-center-enter-active, .firm-center-leave-active {
     transition: opacity .2s ease-in-out;
@@ -85,7 +85,7 @@
           <button class="exit-btn" @click="logout">退出</button>
         </div>
         <div class="fr">
-          <el-carousel :interval="4000" type="card" height="70px" indicator-position="none" :autoplay="false" style="margin-top: 5px;width: 200px;">
+          <el-carousel :interval="4000" type="card" height="64px" indicator-position="none" :autoplay="false" style="margin-top: 8px;width: 200px;">
             <el-carousel-item v-for="item in 3" :key="item">
               <h3><img :src="imgs[item]" alt=""></h3>
             </el-carousel-item>
@@ -111,7 +111,11 @@
     computed: mapGetters(['firmUser']),
     data() {
       return{
-        imgs: ["","http://pic36.photophoto.cn/20150813/0022005593429109_b.jpg", "http://img.zcool.cn/community/01702e568643f66ac7251bb6e5d788.jpg@1280w_1l_2o_100sh.jpg","http://img.zcool.cn/community/038cf1b55deb85500000159959a7d0e.jpg"]
+        imgs: ["",
+          "http://pic33.nipic.com/20130909/13125244_122912091315_2.jpg",
+          "http://img.zcool.cn/community/0381a1856d79d2732f875520fd7b4c3.jpg",
+          "http://img0.imgtn.bdimg.com/it/u=4063375243,1657536129&fm=27&gp=0.jpg"
+        ]
       }
     },
     methods: {
--- pages/firm/register/item.vue
+++ pages/firm/register/item.vue
@@ -1,16 +1,17 @@
 <template>
 	<div class="regBox">
-		<steps :active="4" :stepsArr="stepsArr"></steps>
+		<router-link tag="div" class="back-tip" to="/firm/register/step5">返回</router-link>
 		<el-form :model="form" ref="form" :rules="rules" label-position="left" label-width="0px" class="form form2">
 			<el-form-item prop="loginName">
 				<el-input placeholder="输入项目名称" v-model="form.name">
 					<template slot="prepend">
-						<i class="icon icon5"></i>
+						<i class="icon icon14"></i>
 					</template>
 				</el-input>
 			</el-form-item>
-			<el-form-item prop="nature">
-				<el-select placeholder="选择项目类型" v-model="form.nature" type="password" style="display: block;width: 100%;">
+			<el-form-item prop="nature"style="position:relative">
+				<span class="select-icon"><i class="icon icon15"></i></span>
+				<el-select placeholder="选择项目类型" v-model="form.nature" type="password" style="display: block;width: 100%;padding-left: 46px;">
 					<el-option v-for="item in companyNatures"
 					           :key="item.value"
 					           :label="item.label"
@@ -21,21 +22,21 @@
 			<el-form-item prop="areaId">
 				<el-input placeholder="项目管理员账号" v-model="form.areaId" type="password">
 					<template slot="prepend">
-						<i class="icon icon8"></i>
+						<i class="icon icon16"></i>
 					</template>
 				</el-input>
 			</el-form-item>
 			<el-form-item prop="address">
 				<el-input placeholder="项目管理员密码，建议填写固定电话" v-model="form.address" type="password">
 					<template slot="prepend">
-						<i class="icon icon9"></i>
+						<i class="icon icon17"></i>
 					</template>
 				</el-input>
 			</el-form-item>
 			<el-form-item prop="contact">
 				<el-input placeholder="管理员手机号码，供找回密码和信息推送" v-model="form.contact" type="password">
 					<template slot="prepend">
-						<i class="icon icon10"></i>
+						<i class="icon icon13"></i>
 					</template>
 				</el-input>
 			</el-form-item>
@@ -47,16 +48,16 @@
 				</el-input>
 			</el-form-item>
 			<el-form-item prop="adminName">
-				<el-input placeholder="管理员姓名(建议填写固定电话号码)" v-model="form.adminName" type="password">
+				<el-input placeholder="选择项目地址（省/市/区县）" v-model="form.adminName" type="password">
 					<template slot="prepend">
-						<i class="icon icon12"></i>
+						<i class="icon icon8"></i>
 					</template>
 				</el-input>
 			</el-form-item>
 			<el-form-item prop="tel">
-				<el-input placeholder="手机号码(供找回密码和信息推送用途)" v-model="form.tel" type="password">
+				<el-input placeholder="输入详细地址" v-model="form.tel" type="password">
 					<template slot="prepend">
-						<i class="icon icon13"></i>
+						<i class="icon icon9"></i>
 					</template>
 				</el-input>
 			</el-form-item>
@@ -82,109 +83,45 @@
 		background-color: #fff;
 	}
 </style>
-<style>
-	.regBox {
-		width: 1180px;
-		padding-bottom: 120px;
-		margin: 0 auto;
-		background: #fff;
-		box-shadow: 1px 1px 1px #dee2e9;
-		border-radius: 6px;
+<style scoped lang="less">
+	.regBox{
+		position: relative;
+		overflow: hidden;
 	}
-	.regBox .form {
-		width: 524px;
-		margin: 0 auto;
-		margin-top: 38px;
-	}
-	.block-btn{
-		width: 100%;
-	}
-	.btn-mt {
-		margin-top: 25px;
-	}
-	i.icon{
-		display: inline-block;
-	}
-	i.icon1{
-		width: 17px;
-		height: 17px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 0 0;
-	}
-	i.icon2{
-		width: 17px;
-		height: 17px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 0 -20px;
-	}
-	i.icon3{
-		width: 17px;
-		height: 18px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 0 -40px;
-	}
-	i.icon4{
-		width: 22px;
-		height: 16px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 0 bottom;
-	}
-	i.icon5{
-		width: 22px;
-		height: 22px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 0 -60px;
-	}
-	i.icon6{
-		width: 22px;
-		height: 18px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 4px -80px;
-	}
-	i.icon7{
-		width: 22px;
-		height: 18px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 0 -99px;
-	}
-	i.icon8{
-		width: 22px;
-		height: 20px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 2px -118px;
-	}
-	i.icon9{
-		width: 22px;
-		height: 18px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 0 -139px;
-	}
-	i.icon10{
-		width: 22px;
-		height: 18px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 0 -158px;
-	}
-	i.icon11{
-		width: 22px;
-		height: 18px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 0 -176px;
-	}
-	i.icon12{
-		width: 22px;
-		height: 18px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 0 -195px;
-	}
-	i.icon13{
-		width: 22px;
-		height: 22px;
-		background: url("../../../assets/icon/reg-icon.png") no-repeat 2px -214px;
-	}
-	/*form1*/
-	.regBox .form1 {
-		margin-top: 104px;
-	}
-	.regBox .goLogin {
-		margin-top: 116px;
-	}
-	.regBox .goLogin i{
-		margin-bottom:  -3px;
-		margin-right: 18px;
-	}
-	.regBox .goLogin span {
-		color: #429ffe;
+	.back-tip{
+		position: absolute;
+		top: 0;
+		width: 96px;
+		background: #fda11b;
+		color: #fff;
+		height: 96px;
+		border-radius: 50%;
+		line-height: 134px;
+		text-align: center;
+		left: 50%;
+		transform: translate(-50%,-50%);
+		transition: all 0.2s ease-in-out;
 		cursor: pointer;
+
+		&:hover{
+			background: #fdb63c;
+		 }
 	}
+	.form{
+		padding-top: 60px;
+	}
+	.select-icon{
+		position:absolute;
+		left:0;
+		width: 48px;
+		height: 40px;
+		text-align: center;
+		border: 1px solid #dcdfe7;
+		border-radius: 4px 0 0 4px;
+	}
+</style>
+<style lang="less">
+	@import "register.less";
 </style>
 <script type="text/ecmascript-6">
 	import Steps from '~/components/steps/step.vue';
