dd08914657e276d500a57cf8e13af4ae7ce17ce6
husilang <601906911@qq.com>
2018-04-15 22:28:37 +0800

    个人中心简历

---
+++ pages/client/center/resume.vue
@@ -0,0 +1,13 @@
+<style lang="less" scoped>
+
+</style>
+<template>
+  <div class="resume-pane">
+
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+
+  }
+</script>
---
+++ pages/client/resume-preview.vue
@@ -0,0 +1,9 @@
+<style lang="less" scoped>
+
+</style>
+<template>
+
+</template>
+<script type="text/ecmascript-6">
+
+</script>
